23662e72382544e68ddf27c0ad606a82592e8fc9
Peter Zheng <pzheng@yahoo-inc.com>
2017-09-16 18:01:23 -0700

    optimize syntax

--- Hyphenate-Demo-Swift/Code/Controllers/Chats/EMChatViewController.swift
+++ Hyphenate-Demo-Swift/Code/Controllers/Chats/EMChatViewController.swift
@@ -590,17 +590,17 @@ class EMChatViewController: UIViewController, EMChatToolBarDelegate, EMChatManag
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data, error == nil else {                                                 // check for fundamental networking error
-                print("error=\(error)")
+                print("error=\(error ?? "Fundamental networking error" as! Error)")
                 return
             }
             
             if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
                 print("statusCode should be 200, but is \(httpStatus.statusCode)")
-                print("response = \(response)")
+                print("response = \(String(describing: response))")
             }
             
             let responseString = String(data: data, encoding: .utf8)
-            print("responseString = \(responseString)")
+            print("responseString = \(String(describing: responseString))")
             if (responseString == "{}\n") {
                 let ac = UIAlertController(title: "Safe Website", message: "This website is safe", preferredStyle: .alert)
                 ac.addAction(UIAlertAction(title:"OK", style: .default, handler: nil))
